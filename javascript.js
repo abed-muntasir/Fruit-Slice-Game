e52952132e5501778190158ec9f92371ec17e405
abed-muntasir <muntasir.abed01@gmail.com>
2018-08-02 23:57:31 +0100

    added logic to display score and trials left

--- javascript.js
+++ javascript.js
@@ -1,116 +1,40 @@
 var playing = false;
-var score, action, timeremaining, correctAnswer;
+var score;
+var trialsleft;
+var step;
+var action;
 
-document.getElementById('startreset').onclick = function () {
+$(function () {
 
-    if (playing == true) {
+    $('#startreset').click(function () {
 
-        location.reload();
+        if (playing === true) {
 
-    } else {
+            // reload page
+            location.reload();
+        } else {
 
-        playing = true;
-        score = 0;
-        document.getElementById('scorevalue').innerHTML = score;
+            // we are not playing
+            playing = true; // game initiated
 
-        show("timeremaining");
-        timeremaining = 60;
-        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+            score = 0;
+            $('#scorevalue').html(score);
 
-        hide("gameOver");
-        document.getElementById('startreset').innerHTML = 'Reset game';
+            $('#trialsleft').show();
+            trialsleft = 3;
+            addHearts();
 
-        startCountdown();
+            $('#gameOver').hide();
 
-        generateQA();
-    }
-}
-
-for (i = 1; i < 5; i++) {
-    document.getElementById('box' + i).onclick = function () {
-        if (playing == true) {
-            console.log(correctAnswer);
-            if (this.innerHTML == correctAnswer) {
-                
-                score++;
-
-                document.getElementById('scorevalue').innerHTML = score;
-
-                console.log("inside");
-                hide('wrong');
-                show('correct');
-                setTimeout(function () {
-                    hide('correct')
-                }, 1000);
-
-                generateQA();
-            } else {
-                hide('correct');
-                show('wrong');
-                setTimeout(function () {
-                    hide('wrong')
-                }, 1000);
-            }
+            $('#startreset').html('Reset Game');
         }
-    }
-}
+    });
 
-function startCountdown() {
-    action = setInterval(function () {
+});
 
-        timeremaining -= 1;
-
-        document.getElementById('timeremainingvalue').innerHTML = timeremaining;
-        if (timeremaining == 0) {
-
-            stopCountdown();
-            show('gameOver');
-            document.getElementById('gameOver').innerHTML = "<p>Game over!</p> <p>Your score is " + score + ".</p>";
-
-            hide("timeremaining");
-            hide("correct");
-            hide("wrong");
-            playing = false;
-
-            document.getElementById('startreset').innerHTML = 'Start Game';
-        }
-    }, 1000)
-}
-
-function stopCountdown() {
-    clearInterval(action);
-}
-
-function hide(Id) {
-    document.getElementById(Id).style.display = 'none';
-}
-
-function show(Id) {
-    document.getElementById(Id).style.display = 'block';
-}
-
-function generateQA() {
-    var x = 1 + Math.round(9 * Math.random());
-    var y = 1 + Math.round(9 * Math.random());
-    correctAnswer = x * y;
-
-    document.getElementById('question').innerHTML = x + "x" + y;
-    var correctPosition = 1 + Math.round(3 * Math.random());
-
-    document.getElementById('box' + correctPosition).innerHTML = correctAnswer; //fill one box with the correct answer
-
-    var answers = [correctAnswer];
-
-    for (i = 1; i < 5; i++) {
-        if (i !== correctPosition) {
-            var wrongAnswer;
-            do {
-                var wrongAnswer = (1 + Math.round(9 * Math.random())) * (1 + Math.round(9 * Math.random())); //a wrong answer
-            } while (answers.indexOf(wrongAnswer) > -1)
-
-            document.getElementById('box' + i).innerHTML = wrongAnswer;
-            answers.push(wrongAnswer);
-        }
-
+function addHearts() {
+    $('#trialsLeft').empty();
+    for (i = 0; i < trialsleft; i++) {
+        $('#trialsleft').append('<img src="images/heart.png" class="life">');
     }
 }
