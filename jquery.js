cfae0e3b3c50fff30925b298e34e9c9b9d83f937
abed-muntasir <muntasir.abed01@gmail.com>
2018-08-05 23:52:06 +0100

    added animations for fruit

---
+++ jquery.js
@@ -0,0 +1,90 @@
+var playing = false;
+var score;
+var trialsLeft;
+var fruits = ['apple', 'banana', 'cherries', 'grapes', 'mango', 'orange', 'peach', 'pear', 'watermelon'];
+var step;
+var action;
+
+$(function () {
+
+    $('#startreset').click(function () {
+
+        if (playing === true) {
+
+            // reload page
+            location.reload();
+        } else {
+
+            // we are not playing
+            playing = true; // game initiated
+
+            score = 0;
+            $('#scorevalue').html(score);
+
+            $('#trialsLeft').show();
+            trialsLeft = 3;
+            addHearts();
+
+            $('#gameOver').hide();
+
+            $('#startreset').html('Reset Game');
+            startAction();
+        }
+    });
+
+});
+
+function addHearts(){
+    $("#trialsLeft").empty();
+    for(i = 0; i < trialsLeft; i++){
+        $("#trialsLeft").append('<img src="images/heart.png" class="life">');
+    }
+}
+
+function startAction() {
+
+    $('#fruit1').show();
+    chooseFruit();
+    $('#fruit1').css({ 'left': Math.round(550 * Math.random()), 'top': -50 });
+
+    step = 1 + Math.round(5 * Math.random());
+
+    action = setInterval(function () {
+
+        $('#fruit1').css('top', $('#fruit1').position().top + step);
+
+        if ($('#fruit1').position().top > $('#fruitsContainer').height()) {
+            if (trialsLeft > 1) {
+                $('#fruit1').show();
+                chooseFruit();
+                $('#fruit1').css({ 'left': Math.round(550 * Math.random()), 'top': -50 });
+
+                step = 1 + Math.round(5 * Math.random());
+
+                trialsLeft--;
+
+                addHearts();
+
+            } else {
+                playing = false;
+
+                $('#startreset').html('Start Game');
+                $('#gameOver').show();
+                $('#gameOver').html('<p>Game Over!</p><p>Your score is ' + score +'</p>');
+                $('#trialsLeft').hide();
+                stopAction();
+            }
+        }
+    }, 10);
+}
+
+function chooseFruit() {
+    $('#fruit1').attr('src', 'images/' + fruits[Math.round(8 * Math.random())] + '.png');
+}
+
+
+
+function stopAction(){
+    clearInterval(action);
+    $('#fruit1').hide();
+}
